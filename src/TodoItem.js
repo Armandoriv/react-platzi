e3cbbda71a5f71fb1463f76873d27f1f87023746
unknown <armandoriv98@hotmail.com>
2022-10-25 14:26:38 -0500

    huehueuhe

---
+++ src/TodoItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './TodoItem.css'
+
+function TodoItem(props) {
+    const onComplete = () => {
+        alert('ya completaste el todo '+ props.text);
+    }
+    const onDelete = () => {
+        alert('Borraste el todo '+ props.text);
+
+    }
+
+
+    return (
+        <li className='TodoItem'>
+            <span className={`Icon Icon-check ${props.completed && 'icon-check--active'}`} onClick={onComplete}>
+             √ 
+            </span>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+                {props.text}
+            </p>
+            <span className='Icon Icon-delete' onClick={onDelete}>
+                x
+            </span>
+        </li>
+    );
+
+}
+
+export { TodoItem };
